ac10fe0f74cfb7f1fc61977beff07895a1b9e3cb
TANGRAM <120439883+Hi-Hazle@users.noreply.github.com>
2023-07-28 14:11:29 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import Main from "./pages/main"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <Routes>
+            {/* {routes.map((route, index) => (
+                        <Route key={index} path={route.path} element={route.element}>
+                           {route.children &&
+                              route.children.map((childRoute, childIndex) => (
+                                 <Route key={childIndex} path={childRoute.path} element={childRoute.element} />
+                              ))}
+                        </Route>
+                     ))} */}
+            <Route
+               exact
+               path="/"
+               element={
+                  <>
+                     <Main />
+                  </>
+               }
+            />
+         </Routes>
+      </div>
+   )
 }
 
-export default App;
+export default App
